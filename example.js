fb9e1651ecb932b63dcea747b7b96b5ee6d272bd
Brannen Petit <bpetit940@gmail.com>
2020-06-25 11:44:07 -0500

    updated with burger menu

--- example.js
+++
@@ -1,9 +0,0 @@
-function arrayDoubler(arr) {
-  if (!arr.length) {
-    return;
-  }
-  let newValue = arr[0] * 2;
-  let newArray = [];
-  let returnArray = newArray.push(newValue);
-  return returnArray + arrayDoubler(arr.slice(1));
-}
